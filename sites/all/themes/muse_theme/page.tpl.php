20eb8879bbcea64be19a03aef8651cbd2758beb0
Carlos Munoz Kampff and Xia Amendolara <carlos+xia@>
2017-05-04 09:36:12 -0700

    add page.tpl.php content for Muse theme

---
+++ sites/all/themes/muse_theme/page.tpl.php
@@ -0,0 +1,39 @@
+<div id="root">
+
+	<div class="header-wrap">
+    	<div class="header">
+        	<?php print render($page['title']); ?>
+        	<?php print render($page['header']); ?>
+    	</div>
+	</div>
+  <div class="nav-wrap">
+    <div class="nav">
+
+    </div>
+  </div>
+
+	<div class="slide-show-wrap">
+    	<div class="slide-show">
+
+    	</div>
+	</div>
+	<div class="content-wrap">
+    	<div class="content">
+        	<?php print render($page['content']); ?>
+    	</div>
+	</div>
+
+	<div class="side-bar-first-wrap">
+    	<div class="side-bar-first">
+        	<?php print render($page['sidebar_first']); ?>
+    	</div>
+	</div>
+
+	<div class="footer-wrap">
+    	<div class="footer">
+        	<?php print render($page['footer']); ?>
+    	</div>
+	</div>
+
+</div>
+<!-- root -->
